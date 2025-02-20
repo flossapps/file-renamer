9398f2865575bd6ce806e46c7b60fb4e630df7f6
Carlos <flossapps@atomicmail.io>
2025-02-20 07:32:15 -0800

    Added tooltip for python regex & updated GNU GPL

--- src/file_renamer/ui_form.py
+++ src/file_renamer/ui_form.py
@@ -1,12 +1,8 @@
 """
-File Renamer
+File Renamer is a desktop app for Linux and Windows for batch renaming files.
 https://github.com/flossapps/file-renamer
 
-A desktop app for Linux and Windows for batch renaming files.
-It's Free, Libre, Open Source Software (FLOSS).
-
-Copyright (C) 2024 Carlos
-GNU General Public License
+Copyright (C) 2024 Carlos, GNU General Public License
 https://www.gnu.org/licenses/gpl-3.0.html
 """
 
@@ -256,6 +252,16 @@ class Ui_Widget(object):
         self.search_label.setText(QCoreApplication.translate("Widget", u"Search", None))
         self.replace_label.setText(QCoreApplication.translate("Widget", u"Replace", None))
         self.replace.setText("")
+#if QT_CONFIG(tooltip)
+        self.regex.setToolTip(QCoreApplication.translate("Widget", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
+"</style></head><body style=\" font-family:'Noto Sans'; font-size:12pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Google Python Regex Examples</p></body></html>", None))
+#endif // QT_CONFIG(tooltip)
         self.regex.setText(QCoreApplication.translate("Widget", u"Regular Expression", None))
         self.find_btn.setText(QCoreApplication.translate("Widget", u"Find", None))
     # retranslateUi
